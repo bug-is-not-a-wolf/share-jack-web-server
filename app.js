274f6e1d2b3b1214b4c79e27d723871f0f60aa73
konstantin23 <kostharl@gmail.com>
2016-12-04 03:34:55 +0200

    Useless shit was removed

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
-var http = require('http'),
-    fs   = require('fs'),
-    filePath = 'audio/ADC17605.mp3',
-    stat = fs.statSync(filePath);
+/**
+ * Created by Konstantin on 03.12.2016.
+ */
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require("path");
 
-http.createServer(function(request, response) {
+status = {
+  play: false,
+  pause: false,
+  volume: 1
+};
 
-  response.writeHead(200, {
-    'Content-Type': 'audio/mpeg',
-    'Content-Length': stat.size
-  });
+app.use(express.static('public'));
 
-  // We replaced all the event handlers with a simple call to util.pump()
-  fs.createReadStream(filePath).pipe(response);
-}).listen(8080);
+app.get('/',function(req, res){
+    res.sendFile(path.join(__dirname + '/public/view/admin.html'));
+});
 
-console.log('Server is running...');
+io.on('connection', function(socket){
+    console.log('Connection established...');
+    socket.on('disconnect', function(){
+        console.log('Disconnected...');        
+    });
+    socket.on('play', function () {
+       status.play = true;
+       status.pause = false;
+       console.log('Playing... ' + status.play);
+       io.emit('status', status);
+    });
+    socket.on('pause', function () {
+        status.play = false;
+        status.pause = true;
+        console.log('Stopping... ' + status.pause);
+        io.emit('status', status);
+    });
+    socket.on('volumeChanged', function (data) {
+        status.volume = data;
+        console.log('Volume was changed to ' + data);
+        io.emit('status', status);
+    });
+});
+
+http.listen(8080);
+
+console.log("Running at Port 8080");
