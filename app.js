469440b5327d787b8fd6434f7f936bf32ec34e81
Ruslan Gunawardana <exarus@i.ua>
2016-12-25 22:13:12 +0200

    unneccessful confict resolution fixed

--- app.js
+++ app.js
@@ -1,62 +1,65 @@
-/**
- * Created by Konstantin on 03.12.2016.
- */
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var options = {
-    key: fs.readFileSync('keys/private.key'),
-    cert: fs.readFileSync('keys/certificate.crt')
-};
-var https = require('https').Server(options, app);
-var io = require('socket.io')(https);
-var path = require("path");
+'use strict';
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+const express = require('express');
 
-var status = {
-    play: false,
-    pause: false,
-    volume: 1,
-    currentTime: 0
+const httpsOptions = {
+    key: fs.readFileSync('keys/private.key'),
+    cert: fs.readFileSync('keys/certificate.crt'),
+    ca: fs.readFileSync('keys/ca_bundle.crt')
 };
+
+const app = express();
+const httpServer = http.createServer(app).listen(80);
+const httpsServer = https.createServer(httpsOptions, app).listen(443);
+const io = require('socket.io')(httpsServer);
+
 app.use(express.static('public'));
 
-app.get('/',function(req, res){
+app.all('*', function ensureSecure(req, res, next) {
+    if (req.secure) return next();
+    res.redirect('https://' + req.hostname + req.url);
+});
+
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/view/admin.html'));
 });
 
-io.on('connection', function(socket){
+var status = {
+    isPlaying: false,
+    volume: 1,
+    currentTime: 0	// TODO: update the current time for new clients
+};
+
+io.on('connection', function (socket) {
     console.log('Connection established...');
     console.log(status);
-    //io.emit('status', status);
-    socket.on('disconnect', function(){
+    socket.emit('status', status);
+
+    socket.on('disconnect', function () {
         console.log('Disconnected...');
     });
+
     socket.on('play', function (time) {
-       status.play = true;
-       status.pause = false;
-       status.currentTime = time;
-       console.log('Playing... ' + status.play);
-       io.emit('status', status);
-    });
-    socket.on('pause', function (time) {
-        status.play = false;
-        status.pause = true;
+        status.isPlaying = true;
         status.currentTime = time;
-        console.log('Stopping... ' + status.pause);
+        console.log('Playing... ');
         io.emit('status', status);
     });
-    socket.on('volumeChanged', function (volume , time) {
+
+    socket.on('pause', function (time) {
+        status.isPlaying = false;
+        status.currentTime = time;
+        console.log('Stopping... ');
+        io.emit('status', status);
+    });
+
+    socket.on('volumeChanged', function (volume, time) {
         status.volume = volume;
         status.currentTime = time;
         console.log('Volume was changed to ' + volume);
         io.emit('status', status);
     });
 });
-
-
-
-
-https.listen(3000, function () {
-    console.log("Running at Port 3000");
-});
-
