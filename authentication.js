3c6944db85bebf5867363065f80e958a7c7cdb80
Vladislav Volivec'kiy <Vladislav.Voliveckiy@gmail.com>
2016-12-29 06:10:47 +0000

    Adding mongoDB for authentication

--- authentication.js
+++ authentication.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const express = require('express');
+const mongoose = require('mongoose');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -6,51 +8,21 @@ const cookieParser = require('cookie-parser');
 
 const app = module.exports = express();
 
+const sessionSecret = fs.readFileSync('keys/sessionSecret.key').toString();
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(session({ secret: '1l90QN1re7IQJO3jM3wVS2m-zMNWqonM', resave: false, saveUninitialized: false}));		// TODO: secret
+app.use(session({ secret: sessionSecret, resave: false, saveUninitialized: false}));		// TODO: secret
 app.use(passport.initialize());
 app.use(passport.session());
 
-const adminUser = {id : 1, login : 'admin', password : 'admin'};
+var User = require('./models/user');
 
-passport.serializeUser((user, done) => {
-  console.log('serializeUser', user);
-  done(null, user.id);
-});
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-passport.deserializeUser((id, done) => {
-  console.log('deserializeUser', id);
-//  User.findById(id, function(err, user) {
-//    done(err, user);
-//  });
-  if(id = adminUser.id) {
-    done(null, adminUser);
-  } else {
-    done('err');
-  }
-});
-
-const localStrategy = require('passport-local').Strategy;
-passport.use(new localStrategy(
-  function(username, password, done) {
-    console.log(username, password);
-    const isValidAdmin = (username == adminUser.login && password == adminUser.password);
-    return done(null, isValidAdmin ? adminUser : false );
-}));  // TODO:
-/*  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }*/
-
+mongoose.connect('mongodb://localhost/passport_local_mongoose');
 
 app.post('/login',
   passport.authenticate('local', { successRedirect: '/admin',
@@ -60,4 +32,17 @@ app.post('/login',
 app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
+});
+
+app.post('/register', function(req, res, next) {
+  console.log('registering user');
+  User.register(new User({username: req.body.username}), req.body.password, function(err) {
+    if (err) {
+      console.log('error while user register!', err);
+      res.redirect('/error');
+    } else {
+      console.log('user registered!');
+      res.redirect('/');
+    }
+  });
 });
--- routes.js
+++ routes.js
@@ -4,11 +4,15 @@ const app = module.exports = express();
 app.use(express.static('public'));
 
 // TODO: you still can use '/view/admin.html' from static route...
-app.get('/admin', require('connect-ensure-login').ensureLoggedIn('/'), (req, res) => {
+app.get('/admin', require('connect-ensure-login').ensureLoggedIn('/login'), (req, res) => {
   res.sendFile('/public/view/admin.html', {root: __dirname});
 });
 
 app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
   res.sendFile('/public/view/index.html', {root: __dirname});
 });
 
